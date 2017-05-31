6a36f73d43dde6c2ead9ae1d920a326d4a8f787f
Christopher Patty <christopher.jon.patty@gmail.com>
2017-05-30 22:13:52 -0600

    Updated Readme syntax

--- src/index.js
+++ src/index.js
@@ -26,22 +26,6 @@ function recursivelyBuildObject(obj, k){
   }
 }
 
-var obj = {
-  a: "A string",
-  b: "Another string",
-  c: ["an", "array", "of", "strings", "and", 1, "int"],
-  nested: {
-    a1: "nested",
-    b2: "object of strings",
-    c3: {
-      andA: "deeply",
-      nested: "object"
-    }
-  }
-}
-
-console.log(stringifyObject(obj))
-
 module.exports = {
   stringifyObjectLite: stringifyObject
 }
