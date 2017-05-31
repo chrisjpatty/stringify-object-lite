cf8b22cf21913c8da03289b899144d83a941a64d
Christopher Patty <christopher.jon.patty@gmail.com>
2017-05-30 21:31:22 -0600

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+var string = "";
+var start = "{";
+var end = "}";
+
+function stringifyObject(object){
+  Object.keys(object).forEach(function(key){
+    recursivelyBuildObject(object, key);
+  })
+  //console.log(start + string + end);
+  return start + string + end;
+}
+
+function recursivelyBuildObject(obj, k){
+  if(typeof obj[k] !== "object"){
+    string += k + ": \"" + obj[k] + "\", ";
+  }else{
+    if(Array.isArray(obj[k])){
+      string += k + ": " + '["' + obj[k].join('\",\"') +'"], ';
+    }else{
+      string += k + ": " + start;
+      Object.keys(obj[k]).forEach(function(subKey){
+        recursivelyBuildObject(obj[k], subKey);
+      })
+      string += end + ",";
+    }
+  }
+}
+
+module.exports({
+  stringifyObjectLite: stringify
+})
